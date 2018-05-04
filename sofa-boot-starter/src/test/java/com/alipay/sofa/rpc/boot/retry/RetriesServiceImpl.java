f99c50313dc9a50e46cf412e874657dc9835e917
liwei <lw111072@antfin.com>
2018-05-04 17:48:02 +0800

    remove unnecessary print info

--- sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/retry/RetriesServiceImpl.java
+++ sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/retry/RetriesServiceImpl.java
@@ -34,7 +34,6 @@ public class RetriesServiceImpl implements RetriesService {
         if (current == 3 || current == 6) {
             return string;
         } else {
-            System.out.println("timeout for retries. count[" + count.get() + "]");
             Thread.sleep(3100);
             return "xxx";
         }
