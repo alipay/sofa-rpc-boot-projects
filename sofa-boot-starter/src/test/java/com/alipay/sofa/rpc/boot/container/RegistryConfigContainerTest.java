ea3696f8c30ec3464c5f9e28d86d1329a4a880ad
leizhiyuan <leizhiyuan@gmail.com>
2018-06-14 13:57:02 +0800

    add core project

--- sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/container/RegistryConfigContainerTest.java
+++ sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/container/RegistryConfigContainerTest.java
@@ -16,7 +16,6 @@
  */
 package com.alipay.sofa.rpc.boot.container;
 
-import com.alipay.sofa.rpc.boot.common.SofaBootRpcRuntimeException;
 import com.alipay.sofa.rpc.boot.config.LocalFileConfigurator;
 import com.alipay.sofa.rpc.boot.config.SofaBootRpcProperties;
 import com.alipay.sofa.rpc.boot.config.ZookeeperConfigurator;
