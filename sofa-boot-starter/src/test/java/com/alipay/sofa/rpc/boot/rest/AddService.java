29b5143747c26680f70ee89f8ef1c7cb898224bf
Khotyn Huang <hting1@gmail.com>
2018-12-09 22:26:48 +0800

    Add test case.

--- sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/rest/AddService.java
+++ sofa-boot-starter/src/test/java/com/alipay/sofa/rpc/boot/rest/AddService.java
@@ -14,24 +14,17 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.alipay.sofa.rpc.boot.rest;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 
-/**
- * @author liangen
- * @version $Id: RestService.java, v 0.1 2018年04月15日 下午5:37 liangen Exp $
- */
 @Path("/webapi")
 @Consumes("application/json;charset=UTF-8")
 @Produces("application/json;charset=UTF-8")
-public interface RestService {
+public interface AddService {
 
     @GET
-    @Path("/restService/{id}")
-    String sayRest(@PathParam("id") String string);
+    @Path("/add_service")
+    String sayRest();
 }
